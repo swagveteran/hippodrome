e923dac3e56b2b6b8e3d4949449e1efadef3db72
ichesnokov <deroman23@gmail.com>
2025-07-01 23:51:59 +0700

    refactoring HorseTest

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,14 +1,11 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(MockitoExtension.class)
 public class HorseTest {
 
     private Horse horse;
