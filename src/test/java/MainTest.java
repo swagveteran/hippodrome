0f85169c791ecd033d5a635d5f2d812ca81eca94
ichesnokov <deroman23@gmail.com>
2025-07-02 00:29:14 +0700

    add MainTest

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainTest {
+
+    @Test
+    @Timeout(value = 22, unit = TimeUnit.SECONDS)
+    @Disabled
+    public void mainTest() throws Exception {
+        Main.main(new String[]{});
+    }
+
+}
