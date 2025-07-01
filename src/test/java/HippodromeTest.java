d750ca5049226d6e390e5dbf9a99c3949a3c6d45
ichesnokov <deroman23@gmail.com>
2025-07-02 00:24:33 +0700

    add HippodromeTest

---
+++ src/test/java/HippodromeTest.java
@@ -0,0 +1,69 @@
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HippodromeTest {
+
+    @Test
+    void constructorHippodromeIfParameterAreNull(){
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new Hippodrome(null));
+
+        assertEquals("Horses cannot be null.", exception.getMessage());
+    }
+
+    @Test
+    void constructorHippodromeIfListAreEmpty(){
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new Hippodrome(new ArrayList<>()));
+
+        assertEquals("Horses cannot be empty.", exception.getMessage());
+    }
+
+    @Test
+    void getHorses(){
+        List<Horse> horses = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            horses.add(new Horse("horse" + i, i, i * 0.5));
+        }
+
+        Hippodrome hippodrome = new Hippodrome(horses);
+
+        assertEquals(horses, hippodrome.getHorses());
+
+    }
+
+    @Test
+    void hippodromeMoveTest(){
+        List<Horse> mockHorses = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            mockHorses.add(Mockito.mock(Horse.class));
+        }
+
+        Hippodrome hippodrome = new Hippodrome(mockHorses);
+
+        hippodrome.move();
+
+        for (Horse horseMock : mockHorses) {
+            Mockito.verify(horseMock, Mockito.times(1)).move();
+        }
+    }
+
+    @Test
+    void getWinnerShouldReturnHorseWithMaxDistance() {
+        Horse horse1 = new Horse("Horse1", 10.0, 100.0);
+        Horse horse2 = new Horse("Horse2", 10.0, 150.0); // самая большая дистанция
+        Horse horse3 = new Horse("Horse3", 10.0, 120.0);
+
+        Hippodrome hippodrome = new Hippodrome(List.of(horse1, horse2, horse3));
+
+        Horse winner = hippodrome.getWinner();
+
+        assertSame(horse2, winner, "Метод должен вернуть лошадь с максимальной дистанцией");
+    }
+
+}
